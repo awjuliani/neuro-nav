3ba05288b2bbbb564891a41706d41415417f1eaa
Arthur Juliani <awjuliani@gmail.com>
2022-04-27 08:43:47 -0700

    Update version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ extras_required = {
 
 setup(
     name="neuronav",
-    version="0.4.0",
+    version="0.4.1",
     description="Neuro-Nav",
     license="Apache License 2.0",
     author="Arthur Juliani",
