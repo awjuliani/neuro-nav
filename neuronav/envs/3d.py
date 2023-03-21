f0573b92a674ba301d339dbe6575de80d25aa721
Arthur Juliani <awjuliani@gmail.com>
2023-03-21 18:29:15 -0400

    Render doors, keys, etc

--- neuronav/envs/3d.py
+++ neuronav/envs/3d.py
@@ -4,12 +4,16 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 from PIL import Image
 import time
+import cv2
+import math
 
 from neuronav.envs.grid_env import GridEnv, GridOrientation, GridTemplate
 
 
 def load_texture(filename):
     img = Image.open(filename)
+    img = img.convert("RGB")
+
     img_data = np.array(list(img.getdata()), np.uint8)
 
     texture_id = glGenTextures(1)
@@ -31,7 +35,7 @@ def load_texture(filename):
     return texture_id
 
 
-def render_plane(x, y, z, area, texture_id):
+def render_plane(x, y, z, area, texture_id, repeat=4):
     glEnable(GL_TEXTURE_2D)
     glBindTexture(GL_TEXTURE_2D, texture_id)
 
@@ -44,11 +48,11 @@ def render_plane(x, y, z, area, texture_id):
     # Plane
     glTexCoord2f(0, 0)
     glVertex3f(-half_area, 0.0, half_area)
-    glTexCoord2f(1, 0)
+    glTexCoord2f(repeat, 0)
     glVertex3f(half_area, 0.0, half_area)
-    glTexCoord2f(1, 1)
+    glTexCoord2f(repeat, repeat)
     glVertex3f(half_area, 0.0, -half_area)
-    glTexCoord2f(0, 1)
+    glTexCoord2f(0, repeat)
     glVertex3f(-half_area, 0.0, -half_area)
 
     glEnd()
@@ -57,54 +61,115 @@ def render_plane(x, y, z, area, texture_id):
     glDisable(GL_TEXTURE_2D)
 
 
-def render_cube(x, y, z):
+def render_cube(x, y, z, texture):
     glPushMatrix()
     glTranslatef(x, y, z)
+    glEnable(GL_TEXTURE_2D)
+    glBindTexture(GL_TEXTURE_2D, texture)
     glBegin(GL_QUADS)
 
     # Front face
-    glColor3f(1.0, 0.0, 0.0)
+    glTexCoord2f(0.0, 0.0)
     glVertex3f(-0.5, -0.5, 0.5)
+    glTexCoord2f(1.0, 0.0)
     glVertex3f(0.5, -0.5, 0.5)
+    glTexCoord2f(1.0, 1.0)
     glVertex3f(0.5, 0.5, 0.5)
+    glTexCoord2f(0.0, 1.0)
     glVertex3f(-0.5, 0.5, 0.5)
 
     # Back face
-    glColor3f(0.0, 1.0, 0.0)
+    glTexCoord2f(0.0, 0.0)
     glVertex3f(-0.5, -0.5, -0.5)
+    glTexCoord2f(0.0, 1.0)
     glVertex3f(-0.5, 0.5, -0.5)
+    glTexCoord2f(1.0, 1.0)
     glVertex3f(0.5, 0.5, -0.5)
+    glTexCoord2f(1.0, 0.0)
     glVertex3f(0.5, -0.5, -0.5)
 
     # Left face
-    glColor3f(0.0, 0.0, 1.0)
+    glTexCoord2f(0.0, 0.0)
     glVertex3f(-0.5, -0.5, -0.5)
+    glTexCoord2f(1.0, 0.0)
     glVertex3f(-0.5, -0.5, 0.5)
+    glTexCoord2f(1.0, 1.0)
     glVertex3f(-0.5, 0.5, 0.5)
+    glTexCoord2f(0.0, 1.0)
     glVertex3f(-0.5, 0.5, -0.5)
 
     # Right face
-    glColor3f(1.0, 1.0, 0.0)
+    glTexCoord2f(1.0, 0.0)
     glVertex3f(0.5, -0.5, -0.5)
-    glVertex3f(0.5, 0.5, -0.5)
-    glVertex3f(0.5, 0.5, 0.5)
+    glTexCoord2f(0.0, 0.0)
     glVertex3f(0.5, -0.5, 0.5)
+    glTexCoord2f(0.0, 1.0)
+    glVertex3f(0.5, 0.5, 0.5)
+    glTexCoord2f(1.0, 1.0)
+    glVertex3f(0.5, 0.5, -0.5)
 
     # Top face
-    glColor3f(0.0, 1.0, 1.0)
+    glTexCoord2f(0.0, 1.0)
     glVertex3f(-0.5, 0.5, -0.5)
+    glTexCoord2f(0.0, 0.0)
     glVertex3f(-0.5, 0.5, 0.5)
+    glTexCoord2f(1.0, 0.0)
     glVertex3f(0.5, 0.5, 0.5)
+    glTexCoord2f(1.0, 1.0)
     glVertex3f(0.5, 0.5, -0.5)
 
     # Bottom face
-    glColor3f(1.0, 0.0, 1.0)
+    glTexCoord2f(1.0, 1.0)
     glVertex3f(-0.5, -0.5, -0.5)
-    glVertex3f(0.5, -0.5, -0.5)
-    glVertex3f(0.5, -0.5, 0.5)
+    glTexCoord2f(0.0, 1.0)
     glVertex3f(-0.5, -0.5, 0.5)
+    glTexCoord2f(0.0, 0.0)
+    glVertex3f(0.5, -0.5, 0.5)
+    glTexCoord2f(1.0, 0.0)
+    glVertex3f(0.5, -0.5, -0.5)
 
     glEnd()
+    glDisable(GL_TEXTURE_2D)
+    glPopMatrix()
+
+
+def render_sphere(x, y, z, radius, slices=16, stacks=16, texture=None):
+    glPushMatrix()
+    glTranslatef(x, y, z)
+    if texture is not None:
+        glEnable(GL_TEXTURE_2D)
+        glBindTexture(GL_TEXTURE_2D, texture)
+
+    # Draw the sphere using quad strips
+    for i in range(stacks + 1):
+        lat0 = math.pi * (-0.5 + (i - 1) / stacks)
+        z0 = radius * math.sin(lat0)
+        zr0 = radius * math.cos(lat0)
+
+        lat1 = math.pi * (-0.5 + i / stacks)
+        z1 = radius * math.sin(lat1)
+        zr1 = radius * math.cos(lat1)
+
+        glBegin(GL_QUAD_STRIP)
+        for j in range(slices + 1):
+            lng = 2 * math.pi * (j - 1) / slices
+            x = math.cos(lng)
+            y = math.sin(lng)
+
+            glNormal3f(x * zr0, y * zr0, z0)
+            if texture is not None:
+                glTexCoord2f(j / slices, (i - 1) / stacks)
+            glVertex3f(x * zr0, y * zr0, z0)
+
+            glNormal3f(x * zr1, y * zr1, z1)
+            if texture is not None:
+                glTexCoord2f(j / slices, i / stacks)
+            glVertex3f(x * zr1, y * zr1, z1)
+        glEnd()
+
+    if texture is not None:
+        glDisable(GL_TEXTURE_2D)
+
     glPopMatrix()
 
 
@@ -122,43 +187,144 @@ def set_camera(agent_pos, agent_dir):
     )
 
 
-# Initialize GLFW for off-screen rendering
-glfw.init()
-glfw.window_hint(glfw.VISIBLE, glfw.FALSE)
-window = glfw.create_window(128, 128, "Offscreen", None, None)
-glfw.make_context_current(window)
+def render_walls(blocks, agent_pos, agent_dir):
+    for block in blocks:
+        # check if the block is in the agent's field of view
+        # use the agent's orientation to determine the field of view
+        if agent_dir == 0:
+            if block[0] > agent_pos[0]:
+                continue
+        elif agent_dir == 1:
+            if block[1] < agent_pos[1]:
+                continue
+        elif agent_dir == 2:
+            if block[0] < agent_pos[0]:
+                continue
+        elif agent_dir == 3:
+            if block[1] > agent_pos[1]:
+                continue
+        render_cube(block[0], 0.0, block[1], wall_id)
 
-glClearColor(0.0, 0.5, 1.0, 1.0)
-glEnable(GL_DEPTH_TEST)
-glMatrixMode(GL_PROJECTION)
-glLoadIdentity()
-gluPerspective(45.0, 800 / 600, 0.1, 100.0)
-glMatrixMode(GL_MODELVIEW)
+
+def render_sprite(x, y, z, width, height, texture_id):
+    glEnable(GL_TEXTURE_2D)
+    glBindTexture(GL_TEXTURE_2D, texture_id)
+
+    glPushMatrix()
+    glTranslatef(x, y, z)
+
+    # Align the sprite to face the camera
+    modelview_matrix = glGetFloatv(GL_MODELVIEW_MATRIX)
+    right = np.array([modelview_matrix[0], modelview_matrix[4], modelview_matrix[8]])
+    up = np.array([modelview_matrix[1], modelview_matrix[5], modelview_matrix[9]])
+
+    half_width = width / 2.0
+    half_height = height / 2.0
+
+    vertices = [
+        (-half_width * right) - (half_height * up),
+        (half_width * right) - (half_height * up),
+        (half_width * right) + (half_height * up),
+        (-half_width * right) + (half_height * up),
+    ]
+
+    glBegin(GL_QUADS)
+
+    # Sprite quad
+    glTexCoord2f(0, 0)
+    glVertex3f(vertices[0][0], vertices[0][1], vertices[0][2])
+    glTexCoord2f(1, 0)
+    glVertex3f(vertices[1][0], vertices[1][1], vertices[1][2])
+    glTexCoord2f(1, 1)
+    glVertex3f(vertices[2][0], vertices[2][1], vertices[2][2])
+    glTexCoord2f(0, 1)
+    glVertex3f(vertices[3][0], vertices[3][1], vertices[3][2])
+
+    glEnd()
+    glPopMatrix()
+
+    glDisable(GL_TEXTURE_2D)
+
+
+def initialize_glfw(resolution):
+    glfw.init()
+    glfw.window_hint(glfw.VISIBLE, glfw.FALSE)
+    window = glfw.create_window(resolution, resolution, "Offscreen", None, None)
+    glfw.make_context_current(window)
+    return window
+
+
+def configure_opengl():
+    glClearColor(135 / 255, 206 / 255, 235 / 255, 1.0)
+    glEnable(GL_DEPTH_TEST)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluPerspective(60.0, 1, 0.1, 100.0)
+    glMatrixMode(GL_MODELVIEW)
+
+
+def save_image(image_np, filename):
+    image = Image.fromarray(image_np)
+    image.save(filename)
+
+
+RESOLUTION = 256
+
+window = initialize_glfw(RESOLUTION)
+configure_opengl()
 
 width, height = glfw.get_framebuffer_size(window)
-texture_id = load_texture("floor_tex.jpg")
+tex_folder = "./textures/"
+floor_id = load_texture(f"{tex_folder}floor.png")
+wall_id = load_texture(f"{tex_folder}wall.png")
+gem_id = load_texture(f"{tex_folder}gem.png")
+gem_bad_id = load_texture(f"{tex_folder}gem_bad.png")
+wood_id = load_texture(f"{tex_folder}wood.png")
+key_id = load_texture(f"{tex_folder}key.png")
 
-env = GridEnv(template=GridTemplate.obstacle, orientation_type=GridOrientation.variable)
+env = GridEnv(template=GridTemplate.two_rooms, orientation_type=GridOrientation.fixed)
 env.reset()
 
-# test how long it takes to render 100 frames
+acts = [0, 0, 3, 3, 3, 3, 3, 3, 1, 1, 0, 0, 0, 0, 0]
+
+img_list = []
+
 timer = time.time()
-for i in range(5):
-    env.step(0)
+for i in range(len(acts)):
+    env.step(acts[i])
+
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
     set_camera(env.agent_pos, env.looking)
-
-    for block in env.blocks:
-        render_cube(block[0], 0.0, block[1])
-
-    render_plane(5.0, -0.5, 5.0, 10.0, texture_id)
+    render_walls(env.blocks, env.agent_pos, env.looking)
+    for reward in env.objects["rewards"]:
+        if env.objects["rewards"][reward] > 0:
+            render_sphere(reward[0], 0.0, reward[1], 0.33, texture=gem_id)
+        else:
+            render_sphere(reward[0], 0.0, reward[1], 0.33, texture=gem_bad_id)
+    for door in env.objects["doors"]:
+        render_cube(door[0], 0.0, door[1], wood_id)
+    for key in env.objects["keys"]:
+        render_sphere(key[0], 0.0, key[1], 0.1, texture=key_id)
+    render_plane(5.0, -0.5, 5.0, 10.0, floor_id)
 
     buffer = glReadPixels(0, 0, width, height, GL_RGB, GL_UNSIGNED_BYTE)
     image_np = np.frombuffer(buffer, dtype=np.uint8).reshape(height, width, 3)[::-1]
-    image = Image.fromarray(image_np)
-    image.save(f"test_{i}.png")
+    image_np = np.flip(image_np, axis=1)
+
+    top_down = env.make_visual_obs()
+    top_down = cv2.resize(top_down, (RESOLUTION * 2, RESOLUTION * 2))
+
+    image_np = np.concatenate((image_np, top_down), axis=1)
+
+    img_list.append(image_np)
 
 print(time.time() - timer)
 
 glfw.destroy_window(window)
 glfw.terminate()
+
+import imageio
+
+# save a gif video of the agent's movement
+imageio.mimsave("./out/test.gif", img_list, fps=2)
