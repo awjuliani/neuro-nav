a4dd85f77adb88cd696b7a3b9f9f66a494320971
Arthur Juliani <awjuliani@gmail.com>
2023-06-01 13:28:17 -0400

    Nicer plotting

---
+++ tests/deeprl_test.py
@@ -0,0 +1,6 @@
+from neuronav.deep_agents.agent import BaseAgent
+from neuronav.deep_agents.ppo.agent import PPOAgent
+from neuronav.deep_agents.ppo.model import PPOModel
+from neuronav.deep_agents.sac.agent import SACAgent
+from neuronav.deep_agents.sac.model import SACModel
+import numpy as np
