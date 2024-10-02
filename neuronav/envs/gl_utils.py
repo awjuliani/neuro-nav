f3c04a238685362b1dd2d80aa9b276d651c9eeab
Arthur Juliani <awjuliani@gmail.com>
2024-10-02 11:23:13 -0700

    More code improvement

--- neuronav/envs/gl_utils.py
+++ neuronav/envs/gl_utils.py
@@ -4,14 +4,20 @@ from OpenGL.GLU import *
 from PIL import Image
 import math
 
+# Cache for loaded textures
+texture_cache = {}
+
 
 def load_texture(filename):
+    if filename in texture_cache:
+        return texture_cache[filename]
+
     img = Image.open(filename).convert("RGB")
     img_data = np.array(img, dtype=np.uint8)
 
     texture_id = glGenTextures(1)
     glBindTexture(GL_TEXTURE_2D, texture_id)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
     glTexImage2D(
         GL_TEXTURE_2D,
@@ -24,6 +30,9 @@ def load_texture(filename):
         GL_UNSIGNED_BYTE,
         img_data,
     )
+    glGenerateMipmap(GL_TEXTURE_2D)
+
+    texture_cache[filename] = texture_id
     return texture_id
 
 
@@ -55,69 +64,68 @@ def render_cube(x, y, z, texture):
     glTranslatef(x, y, z)
     glEnable(GL_TEXTURE_2D)
     glBindTexture(GL_TEXTURE_2D, texture)
+
+    vertices = [
+        (-0.5, -0.5, 0.5),
+        (0.5, -0.5, 0.5),
+        (0.5, 0.5, 0.5),
+        (-0.5, 0.5, 0.5),
+        (-0.5, -0.5, -0.5),
+        (-0.5, 0.5, -0.5),
+        (0.5, 0.5, -0.5),
+        (0.5, -0.5, -0.5),
+        (-0.5, 0.5, -0.5),
+        (-0.5, 0.5, 0.5),
+        (0.5, 0.5, 0.5),
+        (0.5, 0.5, -0.5),
+        (-0.5, -0.5, -0.5),
+        (0.5, -0.5, -0.5),
+        (0.5, -0.5, 0.5),
+        (-0.5, -0.5, 0.5),
+        (0.5, -0.5, -0.5),
+        (0.5, 0.5, -0.5),
+        (0.5, 0.5, 0.5),
+        (0.5, -0.5, 0.5),
+        (-0.5, -0.5, -0.5),
+        (-0.5, -0.5, 0.5),
+        (-0.5, 0.5, 0.5),
+        (-0.5, 0.5, -0.5),
+    ]
+
+    tex_coords = [
+        (0, 0),
+        (1, 0),
+        (1, 1),
+        (0, 1),
+        (1, 0),
+        (1, 1),
+        (0, 1),
+        (0, 0),
+        (0, 1),
+        (0, 0),
+        (1, 0),
+        (1, 1),
+        (1, 1),
+        (0, 1),
+        (0, 0),
+        (1, 0),
+        (1, 0),
+        (1, 1),
+        (0, 1),
+        (0, 0),
+        (0, 0),
+        (1, 0),
+        (1, 1),
+        (0, 1),
+    ]
+
     glBegin(GL_QUADS)
-
-    # Front face
-    glTexCoord2f(0.0, 0.0)
-    glVertex3f(-0.5, -0.5, 0.5)
-    glTexCoord2f(1.0, 0.0)
-    glVertex3f(0.5, -0.5, 0.5)
-    glTexCoord2f(1.0, 1.0)
-    glVertex3f(0.5, 0.5, 0.5)
-    glTexCoord2f(0.0, 1.0)
-    glVertex3f(-0.5, 0.5, 0.5)
-
-    # Back face
-    glTexCoord2f(1.0, 0.0)
-    glVertex3f(-0.5, -0.5, -0.5)
-    glTexCoord2f(1.0, 1.0)
-    glVertex3f(-0.5, 0.5, -0.5)
-    glTexCoord2f(0.0, 1.0)
-    glVertex3f(0.5, 0.5, -0.5)
-    glTexCoord2f(0.0, 0.0)
-    glVertex3f(0.5, -0.5, -0.5)
-
-    # Top face
-    glTexCoord2f(0.0, 1.0)
-    glVertex3f(-0.5, 0.5, -0.5)
-    glTexCoord2f(0.0, 0.0)
-    glVertex3f(-0.5, 0.5, 0.5)
-    glTexCoord2f(1.0, 0.0)
-    glVertex3f(0.5, 0.5, 0.5)
-    glTexCoord2f(1.0, 1.0)
-    glVertex3f(0.5, 0.5, -0.5)
-
-    # Bottom face
-    glTexCoord2f(1.0, 1.0)
-    glVertex3f(-0.5, -0.5, -0.5)
-    glTexCoord2f(0.0, 1.0)
-    glVertex3f(0.5, -0.5, -0.5)
-    glTexCoord2f(0.0, 0.0)
-    glVertex3f(0.5, -0.5, 0.5)
-    glTexCoord2f(1.0, 0.0)
-    glVertex3f(-0.5, -0.5, 0.5)
-
-    # Right face
-    glTexCoord2f(1.0, 0.0)
-    glVertex3f(0.5, -0.5, -0.5)
-    glTexCoord2f(1.0, 1.0)
-    glVertex3f(0.5, 0.5, -0.5)
-    glTexCoord2f(0.0, 1.0)
-    glVertex3f(0.5, 0.5, 0.5)
-    glTexCoord2f(0.0, 0.0)
-    glVertex3f(0.5, -0.5, 0.5)
-
-    # Left face
-    glTexCoord2f(0.0, 0.0)
-    glVertex3f(-0.5, -0.5, -0.5)
-    glTexCoord2f(1.0, 0.0)
-    glVertex3f(-0.5, -0.5, 0.5)
-    glTexCoord2f(1.0, 1.0)
-    glVertex3f(-0.5, 0.5, 0.5)
-    glTexCoord2f(0.0, 1.0)
-    glVertex3f(-0.5, 0.5, -0.5)
-
+    for i in range(0, len(vertices), 4):
+        for j in range(4):
+            glTexCoord2f(*tex_coords[i + j])
+            glVertex3f(*vertices[i + j])
     glEnd()
+
     glDisable(GL_TEXTURE_2D)
     glPopMatrix()
 
@@ -130,29 +138,10 @@ def render_sphere(x, y, z, radius, slices=16, stacks=16, texture=None):
         glEnable(GL_TEXTURE_2D)
         glBindTexture(GL_TEXTURE_2D, texture)
 
-    for i in range(stacks):
-        lat0 = math.pi * (-0.5 + float(i) / stacks)
-        z0 = math.sin(lat0)
-        zr0 = math.cos(lat0)
-
-        lat1 = math.pi * (-0.5 + float(i + 1) / stacks)
-        z1 = math.sin(lat1)
-        zr1 = math.cos(lat1)
-
-        glBegin(GL_QUAD_STRIP)
-        for j in range(slices + 1):
-            lng = 2 * math.pi * float(j) / slices
-            x = math.cos(lng)
-            y = math.sin(lng)
-
-            glNormal3f(x * zr0, y * zr0, z0)
-            glTexCoord2f(float(j) / slices, float(i) / stacks)
-            glVertex3f(x * zr0 * radius, y * zr0 * radius, z0 * radius)
-
-            glNormal3f(x * zr1, y * zr1, z1)
-            glTexCoord2f(float(j) / slices, float(i + 1) / stacks)
-            glVertex3f(x * zr1 * radius, y * zr1 * radius, z1 * radius)
-        glEnd()
+    quadric = gluNewQuadric()
+    gluQuadricTexture(quadric, GL_TRUE)
+    gluSphere(quadric, radius, slices, stacks)
+    gluDeleteQuadric(quadric)
 
     if texture is not None:
         glDisable(GL_TEXTURE_2D)
